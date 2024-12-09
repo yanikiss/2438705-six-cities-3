f95ef14d70f32e51dd61f1b4f034d4de5744e580
kosareva.ya <kosareva.ya@skbkontur.ru>
2024-12-10 01:51:36 +0500

    done

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -17,7 +17,8 @@ export class RestApplication {
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
     @inject(Component.OfferController) private readonly offerController: Controller,
     @inject(Component.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
-    @inject(Component.UserController) private readonly userController: Controller
+    @inject(Component.UserController) private readonly userController: Controller,
+    @inject(Component.CommentController) private readonly commentController: Controller
   ) {
     this.server = express();
   }
@@ -42,6 +43,7 @@ export class RestApplication {
   private async _initControllers() {
     this.server.use('/offers', this.offerController.router);
     this.server.use('/users', this.userController.router);
+    this.server.use('/comments', this.commentController.router);
   }
 
   public async _initMiddleware() {
